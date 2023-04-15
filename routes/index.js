fb7d0cbf759781406522f4bab571d5fa5a3b0492
labsterx <labsterx@gmail.com>
2023-04-15 12:10:54 +0900

    updated routes

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 // Require Controllers
 const metaMaskAuth = require('../controllers/metamask-auth');
+const LivepeerVideo = require('../controllers/livepeer-video');
+const LivepeerVideoPricing = require('../controllers/livepeer-video-pricing');
 
 // MetaMask Auth
 router.post('/metamask-signin', metaMaskAuth.signIn);
@@ -18,5 +20,11 @@ router.get('/api/livepeer/asset/:assetid', LivepeerVideo.getLivepeerVideoInfo);
 router.get('/api/livepeer/assets-by-owner/:ownerAddress', LivepeerVideo.getLivepeerVideosByOwner);
 router.post('/api/livepeer/upload-request', LivepeerVideo.createUploadRequest);
 router.get('/api/livepeer/check-asset-status/:assetid', LivepeerVideo.checkLivepeerAssetStatus);
+
+// Livepeer Video Pricing
+router.get('/api/livepeer/pricing/:networkId/:ownerAddress', LivepeerVideoPricing.getPricingByOwnerAddress);
+router.post('/api/livepeer/pricing/add/:networkId/:userAddress', LivepeerVideoPricing.addPricing);
+router.post('/api/livepeer/pricing/update/:networkId/:userAddress', LivepeerVideoPricing.updatePricing);
+router.post('/api/livepeer/pricing/delete/:networkId/:userAddress', LivepeerVideoPricing.deletePricing);
 
 module.exports = router;
